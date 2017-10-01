8174fd9ed8079e0580290fdfcce360aa3571dc4a
Paul Davidson <pauldcdavidson@googlemail.com>
2017-10-01 11:22:42 +0100

    Add files via upload

---
+++ coding/javascript/files/example2/example2.js
@@ -0,0 +1,11 @@
+function ToggleDemo(){
+		
+			if (document.getElementById('demo').style.display=='none'){
+				document.getElementById('demo').style.display='block';
+			}
+			else
+			{
+				document.getElementById('demo').style.display='none';
+			}
+		
+		}
